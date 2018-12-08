127a2cb237662af99a088be33e34c0264044a0f1
Randy's Mac <randydavidl78@gmail.com>
2018-12-07 22:35:05 -0500

    Style doughnut  graph

--- js/graphs.js
+++ js/graphs.js
@@ -8,7 +8,7 @@ void function() {
   let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
-      label: '# of Votes',
+      label: '# of Hits',
       data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
       backgroundColor: 'rgba(116, 119, 191, .3)',
       borderColor: '#7477BF',
@@ -47,7 +47,7 @@ void function() {
   const dailyData =  {
     labels: ["S", "M", "T", "W", "T", "F", "S"],
     datasets: [{
-        label: '# of Votes',
+        label: '# of Hits',
         data: [75, 115, 175, 125, 225, 200, 100],
         backgroundColor: '#7477BF',
         borderWidth: 1
@@ -80,42 +80,41 @@ void function() {
   // mobile user doughnut chart
   const mobileCanvas = document.getElementById("mobile-chart");
 
+  const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+      label: '# of Users',
+      data: [2000, 550, 500],
+      borderWidth: 0,
+      backgroundColor: [
+        '#7477BF',
+        '#78CF82',
+        '#51B6C8'
+      ]
+    }]
+  };
+
+  const mobileOptions = {
+    legend: {
+      position: 'right',
+      reverse: true,
+      labels: {
+        boxWidth: 20,
+        padding: 15,
+        fontSize: 14,
+        fontStyle: 'bold'
+      }
+    },
+    layout: {
+      padding: 15
+    },
+    rotation: 0
+  }
+
+
   let mobileChart = new Chart(mobileCanvas, {
     type: 'doughnut',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-
+    data: mobileData,
+    options: mobileOptions
   });
-
 }();
